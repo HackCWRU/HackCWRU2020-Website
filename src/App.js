0a023043677db5ac5eaed30afe68d923a0822dd2
Chuck Rakaczky <cjr105@case.edu>
2020-01-13 20:19:03 -0500

    added firebase connection

--- src/App.js
+++ src/App.js
@@ -16,15 +16,25 @@ class App extends Component{
 
   render() {
     return (
-      <div className = "App" >
-        <section className = "introduction">
-          <Countdown HackDate = {this.state.HackDate} />
-          <HeaderSection/>
-          <About/>
-          <FAQ/>
-          <Schedule/>
-        </section>
-      </div>
+      <root>
+        <div className = "App" >
+          <section>
+            <Countdown HackDate = {this.state.HackDate} />
+            <HeaderSection/>
+            <About/>
+            <FAQ/>
+            <Schedule/>
+          </section>
+        </div>
+        {/*<!-- The core Firebase JS SDK is always required and must be listed first -->*/}
+        <script src="/__/firebase/7.6.2/firebase-app.js"></script>
+
+        {/*<!-- TODO: Add SDKs for Firebase products that you want to use. https://firebase.google.com/docs/web/setup#available-libraries -->*/}
+        <script src="/__/firebase/7.6.2/firebase-analytics.js"></script>
+
+        {/*<!-- Initialize Firebase -->*/}
+        <script src="/__/firebase/init.js"></script>
+      </root>
     );
   }
 }
